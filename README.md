e87651419e22bbdf831b75d14be17bcaab08632b
David-mwas <dmwas704@gmail.com>
2023-02-23 23:32:42 +0300

    Updating the programme structure adding all dependancies

---
+++ README.md
@@ -0,0 +1,11 @@
+# Java-DSA- a Java code using linked list concept to create a stack and a queue and calculate their time complexity 
+## Time Complexity
+
+ Time complexity of queue operations: O(1) for enqueue, O(n) for dequeue (n is the size of the queue)
+
+Time complexity of stack operations: O(1)
+#### Authors
+David MWANGI   || JOSEPH OMONDI || CYNTHIA DAASHA || SAMUEL MUTUKU 
+    
+
+ 
---
+++ src/Main.java
@@ -0,0 +1,31 @@
+/*
+    David MWANGI   SIT/B/01-02286/2021
+    JOSEPH OMONDI  SIT/B/01-02668/2021
+    CYNTHIA DAASHA SIT/SIT/B/01-02279/2021
+    SAMUEL MUTUKU  SIK/B/01-01715/2021
+*/
+public class Main {
+    public static void main(String[] args) {
+/*        Mmustlinkedin m = new Mmustlinkedin();
+        m.add(12);
+        m.add(13);
+        m.add(14);
+        m.add(15);
+        m.add(16);
+        m.add(17);
+        m.print();
+        m.remove(15);*/
+        /*Stack s = new Stack();
+        s.push(12);
+        s.push(13);
+        s.push(14);
+        s.push(15);
+        System.out.println(s.pop());*/
+        Queue s = new Queue();
+        s.enqueue(12);
+        s.enqueue(13);
+        s.enqueue(14);
+        s.enqueue(15);
+        System.out.println(s.isEmpty());
+    }
+}
